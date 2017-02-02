b420f33aa5e8eec626faa40111148ddd9359bc86
cristhian <durancristhian@gmail.com>
2017-02-01 22:03:23 -0300

    Add eslint + husky

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-const got = require('got')
+const got = require('got');
+
 if (!global.Intl) {
-  global.Intl = require('intl')
+    global.Intl = require('intl');
 }
-const IntlRelativeFormat = require('intl-relativeformat')
-const rf = new IntlRelativeFormat('en')
+
+const IntlRelativeFormat = require('intl-relativeformat');
+const rf = new IntlRelativeFormat('en');
+
 got('https://api.meetup.com/burgerjs/events')
     .then(r => JSON.parse(r.body)[0])
     .then(r => {
-      const nextIn = rf.format(new Date(r.time + r.utc_offset))
-      console.log(`The next BurgerJS is ${nextIn}`)
-      console.log(r.link)
+        const nextIn = rf.format(new Date(r.time + r.utc_offset));
+
+        // eslint-disable-next-line no-console
+        console.log(`The next BurgerJS is ${nextIn}`);
+        // eslint-disable-next-line no-console
+        console.log(r.link);
     })
     .catch(error => {
-        console.log(error.response.body)
-    })
+        // eslint-disable-next-line no-console
+        console.error(error.response.body);
+    });
