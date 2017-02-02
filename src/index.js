b2e954ccfc39ad2edc431d8db7d2d0f88cb24112
cristhian <durancristhian@gmail.com>
2017-02-01 22:49:37 -0300

    Change the output

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+const colors = require('colors');
 const got = require('got');
+const logo = require('burgerjs-logo');
 
 if (!global.Intl) {
     global.Intl = require('intl');
@@ -8,14 +10,20 @@ const IntlRelativeFormat = require('intl-relativeformat');
 const rf = new IntlRelativeFormat('en');
 
 got('https://api.meetup.com/burgerjs/events')
-    .then(r => JSON.parse(r.body)[0])
-    .then(r => {
-        const nextIn = rf.format(new Date(r.time + r.utc_offset));
+    .then(res => JSON.parse(res.body)[0])
+    .then(event => {
+        const missingTime = rf.format(new Date(event.time + event.utc_offset));
+        const message = `\nThe next burger.js is ${missingTime}`;
 
-        // eslint-disable-next-line no-console
-        console.log(`The next BurgerJS is ${nextIn}`);
-        // eslint-disable-next-line no-console
-        console.log(r.link);
+        logo.print();
+
+        setTimeout(() => {
+            // eslint-disable-next-line no-console
+            console.log(colors.yellow.bold(message));
+
+            // eslint-disable-next-line no-console
+            console.log(`More info: ${event.link}\n`);
+        }, 100);
     })
     .catch(error => {
         // eslint-disable-next-line no-console
