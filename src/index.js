fb41073359ca0a89af120a5ef4c2b1b213be32c2
cristhian <durancristhian@gmail.com>
2017-02-22 02:14:32 -0300

    Add list of atendees to the output

--- src/index.js
+++ src/index.js
@@ -9,26 +9,46 @@ if (!global.Intl) {
 const IntlRelativeFormat = require('intl-relativeformat');
 const rf = new IntlRelativeFormat('es-ES');
 
+const handleError = function (error) {
+    // eslint-disable-next-line no-console
+    console.error(error.response.body);
+};
+
 got('https://api.meetup.com/burgerjs/events')
     .then(res => JSON.parse(res.body)[0])
     .then(event => {
-        if (!event){
+        if (!event) {
             // eslint-disable-next-line no-console
-            console.log('Todavía no hay una próxima BurgerJS programada :(');
+            console.log('No hay una Burger.js programada :(');
+
+            // eslint-disable-next-line no-console
+            console.log(colors.yellow.bold('¿Por qué no nos mandas un tweet a @burgerjsba?'));
+
             return;
         }
-        const missingTime = rf.format(new Date(event.time + event.utc_offset));
-        const message = `\nEl próximo burger.js es ${missingTime}`;
 
-        logo.print();
+        got(`https://api.meetup.com/burgerjs/events/${event.id}/rsvps`)
+            .then(res => JSON.parse(res.body))
+            .then(rsvps => rsvps.filter(rsvp => rsvp.response === 'yes'))
+            .then(rsvps => {
+                const missingTime = rf.format(new Date(event.time + event.utc_offset));
 
-        // eslint-disable-next-line no-console
-        console.log(colors.yellow.bold(message));
+                logo.print();
 
-        // eslint-disable-next-line no-console
-        console.log(`Más info: ${event.link}\n`);
+                // eslint-disable-next-line no-console
+                console.log(colors.yellow.bold(`\nEl próximo burger.js es ${missingTime}`));
+
+                // eslint-disable-next-line no-console
+                console.log(`Más info: ${event.link}\n`);
+
+                if (rsvps.length) {
+                    // eslint-disable-next-line no-console
+                    console.log('Los valientes anotados por ahora son:');
+
+                    // eslint-disable-next-line no-console
+                    rsvps.map(rsvp => console.log(`${rsvp.member.name} ${rsvp.guests > 0 ? colors.green('+ ' + rsvp.guests + ' invitados') : ''}`));
+                }
+            })
+            .catch(handleError);
     })
-    .catch(error => {
-        // eslint-disable-next-line no-console
-        console.error(error.response.body);
-    });
+    .catch(handleError);
